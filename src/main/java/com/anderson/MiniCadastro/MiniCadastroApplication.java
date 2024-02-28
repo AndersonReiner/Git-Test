73550081f4665c56eec440168d52d2e23bb47e41
AndersonReiner <reiner.academico@gmail.com>
2024-02-27 20:38:42 -0400

    Primeiro commit

---
+++ src/main/java/com/anderson/MiniCadastro/MiniCadastroApplication.java
@@ -0,0 +1,13 @@
+package com.anderson.MiniCadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniCadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniCadastroApplication.class, args);
+	}
+
+}
